4f50c6a26a6e7f5fc46c16284c58b2943df707f6
Ivan Yuriev <Ivan.Yuriev@avionero.com>
2019-03-01 14:43:46 +0300

    Add project files.

---
+++ TripDatesGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TripDatesGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
